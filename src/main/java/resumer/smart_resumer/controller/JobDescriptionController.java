a8d5809725a9c7e339a296f2ab7d0631e7443c8d
Rohit Prashar <rohitprashar712@gmail.com>
2025-04-13 11:02:11 +0100

    fixing build problems

--- src/main/java/resumer/smart_resumer/controller/JobDescriptionController.java
+++ src/main/java/resumer/smart_resumer/controller/JobDescriptionController.java
@@ -1,5 +1,6 @@
 package resumer.smart_resumer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/jd")
 public class JobDescriptionController {
 
+    @GetMapping
     public String getHello(){
         return "Hello World";
     }
